2468b6a2d5025c291a855f0ccd14397e915e0034
Cody Albert <cody.j.albert@gmail.com>
2014-01-23 21:56:36 -0500

    Added some gameplay code and mocked up a simple demo

---
+++ Assets/Scripts/LightOrb.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightOrb : MonoBehaviour {
+	
+	float timeRemaining;
+
+	// Use this for initialization
+	void Start () {
+		timeRemaining = Settings.getInstance().OrbTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timeRemaining -= Time.deltaTime;
+		Color color = renderer.material.color;
+		color.a = timeRemaining / Settings.getInstance().OrbTime;;
+		renderer.material.color = color;
+		if(timeRemaining < 0){
+			LightController.OrbCount--;
+			DestroyImmediate(this.gameObject);
+		}
+
+	}
+
+	void OnMouseOver(){
+		if(Input.GetMouseButtonDown(1)){
+			GameObject player = GameObject.FindGameObjectWithTag("Player");
+			player.GetComponent<CharacterController>().Move(transform.position - player.transform.position);
+			LightController.OrbCount--;
+			DestroyImmediate(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Settings : MonoBehaviour{
+
+	public float OrbTime;
+	
+	public float DashSpeed;
+
+	public int MaxOrbs;
+
+	static Settings instance;
+
+	void Awake(){
+		instance = this;
+	}
+
+	public static Settings getInstance ()
+	{
+		return instance;
+	}
+
+
+
+}
