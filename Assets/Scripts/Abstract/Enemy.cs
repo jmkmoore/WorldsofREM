e0b5cffdc79f52385885ff678fb77629eb109ee7
Joshua Moore <jmkmoore@gmail.com>
2014-10-18 17:19:26 -0400

    Merge remote-tracking branch 'origin/master' into projectile
    Conflicts:
    	Assets/Scenes/TestScene.unity
    	Library/CurrentLayout.dwlt
    	Library/InspectorExpandedItems.asset
    	Library/ScriptAssemblies/Assembly-CSharp-firstpass.dll
    	Library/ScriptAssemblies/Assembly-CSharp-firstpass.dll.mdb
    	Library/ScriptAssemblies/Assembly-CSharp.dll
    	Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    	Library/ScriptAssemblies/Assembly-UnityScript-firstpass.dll
    	Library/ScriptAssemblies/Assembly-UnityScript-firstpass.dll.mdb
    	Library/assetDatabase3
    	Library/expandedItems
    	Library/guidmapper
    	Library/metadata/06/0680c2e95a897e8469f9c04369cab194
    	Library/metadata/43/436ede1c61e58f54ea174ad0b6d80b52
    	Library/metadata/af/af23e14fddca6044a94a494dc4dd8702
    	Library/metadata/f2/f2d85546877ae5246a7706aff3a0b768
    	TienJam-csharp.sln
    	TienJam.sln
    	TienJam.userprefs

--- Assets/Scripts/Abstract/Enemy.cs
+++ Assets/Scripts/Abstract/Enemy.cs
@@ -6,6 +6,7 @@ public abstract class Enemy : TienEntity {
 	
 	
 	public static TienEntity CreateEnemy(Dictionary<string,string> props){
+		Debug.Log (props.ContainsKey("EnemyType"));
 		GameObject entity = GameObject.Instantiate(Resources.Load("Prefab/Objects/Enemy/"+props["EnemyType"])) as GameObject; 
 		TienEntity enemy = entity.GetComponent<TienEntity>();
 		enemy.properties = props;
